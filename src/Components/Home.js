f6d2f95ec8a649db0d3c15c6dc800a046eb50b73
Adam Castro <ajcastr1@uiwtx.edu>
2024-06-03 22:30:23 -0500

    projects page in progress

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,29 +3,60 @@ import { Link } from "react-router-dom";
 import Type from "./Type";
 import Tilt from "react-parallax-tilt";
 import Avatar from "../images/avatar.png";
-import { CgPhone } from "react-icons/cg";
-import { BsPerson } from "react-icons/bs";
+import { AiOutlineFileText, AiOutlineProject } from "react-icons/ai";
 
 const Home = () => {
   return (
-    <div>
+    <div className="container">
       <nav className="navbar">
-        <Link to="/about">
-          <button>About Me <BsPerson /></button>
+        <Link to="/resume">
+          <button>
+            Resume <AiOutlineFileText />
+          </button>
         </Link>
-        <Link to="/contact">
-          <button>Contact <CgPhone /></button>
+        <Link to="/project">
+          <button>
+            Projects <AiOutlineProject />
+          </button>
         </Link>
       </nav>
       <div className="home-content">
-        <div className="home-text">
-          <h1>Hello there!</h1>
-          <h1>I'm <b>Adam Castro</b></h1>
-          <Type />
-        </div>
         <Tilt>
           <img className="avatar" src={Avatar} alt="Avatar" />
         </Tilt>
+        <div className="home-text">
+          <h1>Adam Castro</h1>
+          <Type />
+        </div>
+      </div>
+      <div className="home-extra">
+        <div className="about-section">
+          <h3>About Me</h3>
+          <p>
+            Hello! I'm Adam, a web and mobile technology enthusiast at the
+            University of the Incarnate Word. I specialize in transforming
+            digital interactions with my expertise in JavaScript, React, NodeJS,
+            and AWS. My experience spans across various tools and technologies,
+            including Git, Azure, GraphQL, and MaterialUI. As a new father,
+            balancing family life with my career has honed my creativity and
+            problem-solving skills. I thrive on collaboration and believe the
+            best tech solutions are intuitive and user-friendly.
+          </p>
+        </div>
+        <div className="contact-section">
+          <h3>Contact</h3>
+          <ul className="contact-links">
+            <li>
+              <a href="https://twitter.com">Twitter</a>
+            </li>
+            <li>
+              <a href="https://linkedin.com">LinkedIn</a>
+            </li>
+            <li>
+              <a href="https://facebook.com">Facebook</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import { AiOutlineFileText, AiOutlineProject } from "react-icons/ai";
 
 const Project = () => {
   return (
     <div>
+      <nav className="navbar">
+        <Link to="/resume">
+          <button>
+            Resume <AiOutlineFileText />
+          </button>
+        </Link>
+        <Link to="/project">
+          <button>
+            Projects <AiOutlineProject />
+          </button>
+        </Link>
+      </nav>
       <h1>Project Page</h1>
+      
     </div>
   );
 };
